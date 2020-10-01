a85f0a4a327796086ac0b4db1d340120ac843c31
JustNunuz <nunudzaimrewa222@gmail.com>
2020-10-02 02:26:03 +0800

    Update reverse_name.py
    I added an option to reverse both the name and surname together

--- Python/reverse_name.py
+++ Python/reverse_name.py
@@ -1,3 +1,29 @@
-first = input("Enter first name")
-last = input("Enter last name")
-print(first[::-1]+" " +last[::-1])
+print("Welcome to the reverse Name Code")
+print("Enter your name as we will reverse it.")
+print("Choose option 1 or 2")
+choice=input("Choose your technique")
+first = input("Enter first name:")
+last = input("Enter last name:")
+
+def reverseWord(first,last):
+    print("The reversed message is " + first[::-1]+" " +last[::-1])
+
+def reverseEverything():
+    message=first+" "+ last
+    translated=""
+
+    i=len(message)-1
+    #index starts from 0
+
+    while i>=0:
+        translated=translated+message[i]
+        i=i-1
+        
+    print(translated)
+
+if choice==1:
+    reverseWord()
+if choice==2:
+    reverseEverything()
+else:
+    print("Option not available")
